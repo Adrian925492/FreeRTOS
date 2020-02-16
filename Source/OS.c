c1172be44dd073119261b14282c2cae651d53b0d
Adrian925492 <adrian.9254@gmail.com>
2020-02-16 20:00:46 +0100

    Blinky task added

--- Source/OS.c
+++ Source/OS.c
@@ -1,15 +1,27 @@
 #include "stm32f4xx_hal.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "main.h"
+
+#define dBlinkPeriod_ms (1000)
 
 void vIdleTask(void *pvParameters);
-
+void vBlinkyTask(void *pvParaeters);
 
 void vIdleTask(void *pvParameters)
 {
     for(;;)
     {
         //Idle task: do nothing
+    }
+}
+
+void vBlinkyTask(void* pvParameters)
+{
+    for(;;)
+    {
+        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+        vTaskDelay(pdMS_TO_TICKS((int)(pvParameters)));
     }
 }
 
@@ -21,6 +33,7 @@ void os_init(void)
 void os_start(void)
 {
     xTaskCreate(vIdleTask, "IdleTask", 32, NULL, 0, NULL);
+    xTaskCreate(vBlinkyTask, "BlinkyTask", 128, dBlinkPeriod_ms, 1, NULL);
     vTaskStartScheduler();
 
     for(;;);   //Shall be never reached
